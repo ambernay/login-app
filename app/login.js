a2a796e6b9a5fdf5e55cb468836f1a7ab855244b
ambernay <ambernaytrotter@gmail.com>
2023-11-07 16:05:15 -0500

    styled login page, added signup button link

--- app/login.js
+++ app/login.js
@@ -5,7 +5,7 @@ import { Link } from 'expo-router';
 
 const Login = () => {
 
-    const [inputValues, setInputValues] = useState({email: '', username: '', password: ''});
+    const [inputValues, setInputValues] = useState({email: '', password: ''});
     const [hidePassword, setHidePassword] = useState(true);
 
     const buttonHandler = () => {
@@ -15,34 +15,42 @@ const Login = () => {
     return (
         <View name='pageContainer' style={styles.pageContainer}>
             <Text style={styles.headingText}>Chickie Tendies</Text>
-            <Text style={styles.headingText}>Sign Up</Text>
 
-            <View style={styles.formContainer}>
-            <InputField 
-              onChangeText={(newText) => setInputValues({...inputValues, ['email']: newText})} 
-              value={inputValues['email']}
-              placeholder="Email..."
-              iconName='mail'
-            />
-            <InputField
-              value={inputValues['password']} 
-              onChangeText={(newText) => setInputValues({...inputValues, ['password']: newText})} 
-              placeholder="Password..."
-              iconName='lock-closed'
-              textContentType={'password'}
-              hidePassword={hidePassword}
-              setHidePassword={setHidePassword}
-              isPassword={true}
-            />
-            <TouchableOpacity 
-              style={styles.profileButton} 
-              onPress={buttonHandler} 
-            >
-              <Text style={styles.smallText}>
-                Login
-              </Text>
-            </TouchableOpacity>
+            <View style={[styles.container, styles.formContainer]}>
+              <InputField 
+                onChangeText={(newText) => setInputValues({...inputValues, ['email']: newText})} 
+                value={inputValues['email']}
+                placeholder="Email..."
+                iconName='mail'
+              />
+              <InputField
+                value={inputValues['password']} 
+                onChangeText={(newText) => setInputValues({...inputValues, ['password']: newText})} 
+                placeholder="Password..."
+                iconName='lock-closed'
+                textContentType={'password'}
+                hidePassword={hidePassword}
+                setHidePassword={setHidePassword}
+                isPassword={true}
+              />
+              <TouchableOpacity 
+                style={[styles.button, styles.loginButton]} 
+                onPress={buttonHandler} 
+              >
+                <Text style={styles.smallText}>Log In</Text>
+              </TouchableOpacity>
+
+              <View style={[styles.container, styles.signUpContainer]}>
+              <TouchableOpacity 
+                style={[styles.button, styles.signupButton]} 
+                onPress={buttonHandler} 
+              >
+                <Link style={styles.smallText} href='signup_form'>Sign Up</Link>
+              </TouchableOpacity>
+            </View>
+            {/* vv formContainer vv */}
           </View>
+          {/* vv pageContainer vv */}
         </View>
     )
 }
@@ -62,28 +70,46 @@ const styles = StyleSheet.create({
       color: 'white',
       alignSelf: 'center'
     },
-  
-    formContainer: {
+
+    container: {
       display: 'flex',
       flexDirection: 'column',
       rowGap: 15,
       alignSelf: 'center',
       alignItems: 'center',
+      width: '100%'
+    },
+  
+    formContainer: {
+      rowGap: 15,
       height: '60%',
       maxWidth: 300,
       width: '100%',
       paddingTop: 40
     },
+    
+    signUpContainer: {
+      height: '40%',
+      borderTopColor: '#395C5F',
+    },
   
-    profileButton: {
-      backgroundColor: 'white',
-      borderRadius: 25,
+    button: {
+      borderRadius: 20,
       height: 45,
-      width: '50%',
       justifyContent: 'center',
       alignItems: 'center',
       marginTop: 20,
       padding: 10
+    },
+
+    loginButton: {
+      width: '100%',
+      backgroundColor: 'white',
+    },
+
+    signupButton: {
+      width: '50%',
+      backgroundColor: '#6C994C',
     },
   
     smallText: {
